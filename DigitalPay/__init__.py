ea8a8dcab0311b0715eda866b1f23f316af418b7
Mustafah Zacs <86454983+zacUg@users.noreply.github.com>
2022-01-30 12:42:12 +0300

    Add files via upload

---
+++ DigitalPay/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+
+from . import views
+
+def create_app():
+    app = Flask(__name__)
+    app.config["SECRET_KEY"] = "put any long random string here"
+    app.register_blueprint(views.main_bp)
+    return app
+
+from . import auth
+
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config["SECRET_KEY"] = "put any long random string here"
+
+    auth.login_manager.init_app(app)
+
+    app.register_blueprint(views.main_bp)
+    app.register_blueprint(auth.auth_bp)
+
+    return app    
